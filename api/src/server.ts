0554d2479171457a7b4c44ef685e8192cc9a8a7f
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-09-16 18:11:41 -0300

    feat: complete day 2 - back-end application

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,21 @@
 import { fastify } from "fastify";
-import { prisma } from "./lib/prisma";
+import { fastifyCors } from "@fastify/cors";
 import { getAllPromptsRoutes } from "./routes/get-all-prompts";
+import { uploadVideoRoutes } from "./routes/upload-video";
+import { create } from "domain";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
 
+app.register(fastifyCors, {
+    origin: '*',
+})
+
 app.register(getAllPromptsRoutes)
+app.register(uploadVideoRoutes)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
     port: 3333,
